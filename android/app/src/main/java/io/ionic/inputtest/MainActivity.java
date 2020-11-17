6b75eb239d1a60d978264801c7accd3d6414034a
Aparajita Fishman <aparajita@aparajita.com>
2020-11-17 15:58:17 -0800

    Changed appId

--- android/app/src/main/java/io/ionic/inputtest/MainActivity.java
+++ android/app/src/main/java/io/ionic/inputtest/MainActivity.java
@@ -1,4 +1,4 @@
-package io.ionic.starter;
+package io.ionic.inputtest;
 
 import android.os.Bundle;
 
--- src/main.ts
+++ src/main.ts
@@ -22,11 +22,18 @@ import '@ionic/vue/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import { Capacitor } from '@capacitor/core'
+
+const options = { mode: 'ios' }
+
+if (Capacitor.isNative && Capacitor.getPlatform() === 'android') {
+  options.mode = 'md'
+}
 
 const app = createApp(App)
-  .use(IonicVue)
+  .use(IonicVue, options)
   .use(router);
-  
+
 router.isReady().then(() => {
   app.mount('#app');
 });
